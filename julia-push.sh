cde6e27c5840819ab16a2fe9b66a75f1f4e25e3d
Wonjae Lee <wonjae.lee.2@gmail.com>
2022-06-16 15:21:03 +0200

    Update julia-push.sh

--- julia-push.sh
+++ julia-push.sh
@@ -2,7 +2,7 @@
 
 JULIA_VERSION=1.7.3
 PROJECT_ID=project-lee-1
-ARTIFACT_REGISTRY=us-central1-docker.pkg.dev/$PROJECT_ID/lee
+ARTIFACT_REGISTRY=us-central1-docker.pkg.dev/$PROJECT_ID/docker
 
 docker tag lee/julia:$JULIA_VERSION $ARTIFACT_REGISTRY/julia:$JULIA_VERSION
 
