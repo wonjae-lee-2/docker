84d00694e50c10e4cf5e229762f40a2a6f337e16
Ubuntu <ubuntu@ip-172-31-8-31.ec2.internal>
2022-06-16 15:36:42 +0000

    Update the path of the artifact registry.

--- r/gcloud-auth.sh
+++ r/gcloud-auth.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set environment variables.
-SERVICE_ACCOUNT=service-account@glossy-essence-352111.iam.gserviceaccount.com
+SERVICE_ACCOUNT=account-1@project-lee-1.iam.gserviceaccount.com
 KEY_FILE=~/keys/key-gcloud.json
-PROJECT_ID=glossy-essence-352111
+PROJECT_ID=project-lee-1
 CLUSTER_NAME=cluster-1
-CLUSTER_ZONE=us-central1-a
+CLUSTER_ZONE=us-central1-c
 
 # Authenticate with a service account.
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$KEY_FILE --project=$PROJECT_ID
